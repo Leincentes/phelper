fae2ce3167cbfa6861b33415019e26c7df63afe3
leincentes <leincentes@gmail.com>
2023-07-09 21:51:43 +0800

    fixed next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
-    },
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    remotePatterns: [
+        {
+            protocol: "https",
+            hostname: "lh3.googleusercontent.com",
+        },
+    ],
+    minimumCacheTTL: 15000000,
+},
 }
 
 module.exports = nextConfig
